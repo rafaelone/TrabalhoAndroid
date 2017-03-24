2f97b350c4a160c520e2abb119bafaa7387e03da
Casa <casa@localhost.localdomain>
2017-03-24 15:25:41 -0300

    drawer terminado

--- app/src/main/java/br/com/fiap/exercicios/drawerandfragments/RM77722/MainActivity.java
+++ app/src/main/java/br/com/fiap/exercicios/drawerandfragments/RM77722/MainActivity.java
@@ -2,18 +2,48 @@ package br.com.fiap.exercicios.drawerandfragments.RM77722;
 
 import android.content.Intent;
 import android.os.Handler;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 
 public class MainActivity extends AppCompatActivity {
+
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
+    Toolbar toolbar;
+    ActionBarDrawerToggle actionBarDrawerToggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawerlayout);
+        navigationView = (NavigationView) findViewById(R.id.navigationView);
+
+        setSupportActionBar(toolbar);
+
+        actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.abrir, R.string.fechar);
+        actionBarDrawerToggle.syncState();
+
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                if (item.getItemId() == R.id.nmSobre){
+                    Toast.makeText(MainActivity.this, "Sobre", Toast.LENGTH_SHORT).show();
+                }
+
+                return false;
+            }
+        });
 
 
     }
